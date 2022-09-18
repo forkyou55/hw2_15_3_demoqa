410ec8b3600875a93bde51c2dfcc1db0d4cfc9e2
stanislavivanishchev <ivanishevstanislav@gmail.com>
2022-09-18 16:07:45 +0200

    Fixed build.gradle and fineshed homework

---
+++ src/test/java/com/demoqa/PracticeForm.java
@@ -0,0 +1,62 @@
+package com.demoqa;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class PracticeForm {
+    @BeforeAll
+    static void configuration() {
+        Configuration.browser = "chrome";
+        Configuration.baseUrl = "https://demoqa.com";
+    }
+
+    @Test
+    void fillTestes() {
+        open("/automation-practice-form");
+        executeJavaScript("$('footer').remove()");
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('#RightSide_Advertisement').remove()");
+        $("#firstName").setValue("Foma");
+        $("#lastName").setValue("Kiniaev");
+        $("#userEmail").setValue("FKiniaev@gmail.com");
+        $("#genterWrapper").$(byText("Male")).click();
+        $("#userNumber").setValue("9995771202");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("August");
+        $(".react-datepicker__year-select").selectOption("1969");
+        $(".react-datepicker__day--021").click();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+        $("#subjectsInput").setValue("English").pressEnter();
+
+        $("#uploadPicture").uploadFromClasspath("John Doe.jpeg");
+        $("#currentAddress").setValue("Lubyanka Street");
+
+        executeJavaScript("$('footer').remove()");
+        executeJavaScript("$('#fixedban').remove()");
+
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Delhi")).click();
+
+        $("#submit").click();
+
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(text("Foma Kiniaev"),
+                text("FKiniaev@gmail.com"),
+                text("Male"),
+                text("9995771202"),
+                text("21 August,1969"),
+                text("English"),
+                text("Sports"),
+                text("John Doe.jpeg"),
+                text("Lubyanka Street"),
+                text("NCR Delhi"));
+    }
+}
